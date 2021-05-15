9be66db7d181c818a2487ad69c83cb75c2dce18b
ivombi <ivombi97@gmail.com>
2021-05-15 18:05:21 +0200

    parallel not working

--- info-h515-pred.py
+++ info-h515-pred.py
@@ -3,15 +3,17 @@ from line_profiler import LineProfiler
 import PIL
 import PIL.Image
 from PIL import Image
+import os
 import sys
 import json
-
+import pandas as pd
 import torch
 import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
 from torch.utils.data import Dataset, DataLoader
 
+#mpiexec -n 3 python -m mpi4py info-h515-pred.py
  
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
@@ -50,40 +52,56 @@ class Net(nn.Module):
 model = Net()
 
 
-def node0():
-    test_dir= "C:\\Users\\ivomb\\OneDrive\\Msc Data Science\\Second-Semester\\INFO-H-515-BigData-Distributed-Data-Management-and-Scalable-Analytics\\Practical_Sessions\\mini_herbarium\\train\\images\\000\\01\\230965.jpg"
+
+def node0(): 
+    
+    test_dir = "C:\\Users\\ivomb\\OneDrive\\Msc Data Science\\Second-Semester\\INFO-H-515-BigData-Distributed-Data-Management-and-Scalable-Analytics\\Practical_Sessions\\mini_herbarium\\train\\"
     # loading train images metadata
-    #with open(test_dir , "r", encoding="ISO-8859-1") as file:
-    image = Image.open(test_dir)    
-    print('Node 0 sends', image)
+    with open(test_dir + 'metadata.json', "r", encoding="ISO-8859-1") as file:
+        train = json.load(file)
+    df_test = pd.DataFrame(train['images'])
+    df_file = df_test.iloc[:,0] # extracting the file names
+    
+    for i in range(df_file.shape[0]):
+        image_file = df_file[i]
+        print(image_file)
+        pilo_image = Image.open(os.path.join(test_dir, image_file))
+        
+        comm.send(pilo_image,dest=1) 
+       
+    print('Node 0 sent',i, "files")
     sys.stdout.flush() 
-    comm.send(image,dest=1) 
 
     # Tell Node 1 that we are done
     comm.send((None), dest=1)
 
 
 def node1():
-    image = comm.recv(source=0)
-    image = Transform(image)
+    
+    pilo_image = comm.recv(source=0)
+    
+    transform_image = Transform(pilo_image)
+        
+    comm.send(transform_image.unsqueeze(0),dest=2)  #unsqueeze changes the shape of the tensor from 3 to 4
 
-    comm.send(image.unsqueeze(0),dest=2) 
+    # Tell Node 2 that we are done
+    comm.send((None), dest=2)
 
 def node2():
-    while True:
-        image = comm.recv(source=1)
-        if image is None:
-            break
-        else:
-            model_1 = torch.load('model.pth')
-            output = model_1(image)
-            _, predicted = torch.max(output, 1)
-            print(predicted)
+    
+    image = comm.recv(source=1)
+    
+    
+    model_1 = torch.load('model.pth')
+    output = model_1(image)
+    _, predicted = torch.max(output, 1)
+    
+    print(predicted)
 
 
 if rank == 0:
     node0()
 elif rank == 1:
     node1()
-elif rank == 2:
+else:
     node2()
